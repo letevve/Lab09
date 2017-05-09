8865194b3e7c08030402e670bb3f1b92cf632853
franc <franc@DESKTOP-D3B6B30>
2017-05-09 18:04:48 +0200

    Interfaccia grafica

--- Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
+++ Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
@@ -1,5 +1,44 @@
 package it.polito.tdp.metrodeparis;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
 public class MetroDeParisController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<?> cmbPartenza;
+
+    @FXML
+    private ComboBox<?> cmbArrivo;
+
+    @FXML
+    private Button btnPercorso;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doCalcolaPercorso(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert cmbPartenza != null : "fx:id=\"cmbPartenza\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert cmbArrivo != null : "fx:id=\"cmbArrivo\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert btnPercorso != null : "fx:id=\"btnPercorso\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+
+    }
 }
